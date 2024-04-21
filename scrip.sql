f670d52b29724fe4432a792209baec1a7f771aa6
RobinRodríguez2001 <dgero222@gmail.com>
2024-04-20 22:00:41 -0600

    Scrip SQL

---
+++ scrip.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: SELECT * FROM db_empresa.clientes
+LIMIT 0, 1000
+
+-- Date: 2024-04-20 19:56
+*/
+INSERT INTO `` (`id_clientes`,`nit`,`nombres`,`apellidos`,`direccion`,`telefono`,`fecha_nacimiento`) VALUES (1,'4444','Daniel ','Geronimo','Guatemala','101010','2009-01-01');
+INSERT INTO `` (`id_clientes`,`nit`,`nombres`,`apellidos`,`direccion`,`telefono`,`fecha_nacimiento`) VALUES (2,'11234','jose luis','perez lopez','Guatemala','0000','2000-10-01');
+INSERT INTO `` (`id_empleados`,`codigo`,`nombres`,`apellidos`,`direccion`,`telefono`,`fecha_nacimiento`,`id_puesto`) VALUES (1,'E001','pedro','paz mas','Guatemala','5555','1992-01-01 00:00:00',1);
+INSERT INTO `` (`id_empleados`,`codigo`,`nombres`,`apellidos`,`direccion`,`telefono`,`fecha_nacimiento`,`id_puesto`) VALUES (2,'E002','julio','guzman','Guatemala','222','1993-01-01 00:00:00',2);
+INSERT INTO `` (`id_puesto`,`puesto`) VALUES (1,'programador');
+INSERT INTO `` (`id_puesto`,`puesto`) VALUES (2,'administrador');
